030301bd99f7d99802638beb79162fd963f4d8fc
kaave <junkjunctions@gmail.com>
2023-02-25 20:38:14 +0900

    Update

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,18 +1,12 @@
 // Preload images
-const preloadFonts = id => {
-    return new Promise((resolve) => {
-        WebFont.load({
-            typekit: {
-                id: id
-            },
-            active: resolve
-        });
-    });
+export const preloadFonts = (id) => {
+  return new Promise((resolve) =>
+    WebFont.load({
+      typekit: { id },
+      active: resolve,
+    })
+  );
 };
 
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-export {
-    preloadFonts,
-    randomNumber
-}
+export const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
